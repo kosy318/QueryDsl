9d5c316ebef27dc6a9801c3fd994bb243d811f08
고수연 <kosy1782@gmail.com>
2023-05-15 22:10:21 +0900

    feat(BE):  shorten querydsl

--- querydsl/src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import static com.study.querydsl.entity.QMember.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
@@ -57,12 +58,19 @@ class QuerydslBasicTest {
     @Test
     public void startQuerydsl(){
         JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-        QMember m = new QMember("m");
+//        QMember m = new QMember("m"); // 별칭 직접 지정
+//        QMember m = QMember.member; // 기본 인스턴스 사용
+//
+//        Member findMember = queryFactory
+//                .select(m)
+//                .from(m)
+//                .where(m.username.eq("member1")) // 파라미터 바인딩 처리
+//                .fetchOne();
 
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1"))
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
